6ac41bafa168d4de133c0958b94d72714430bde3
VaheC <vcharchian@gmail.com>
2025-12-25 11:46:47 +0400

    initialized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup = (
+    name='GenAI Medical Bot',
+    version='0.0.0',
+    author='Vahe Charchian',
+    author_email='vcharchian@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
