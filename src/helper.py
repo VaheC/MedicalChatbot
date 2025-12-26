e64f9e962cd38753047e5a667a80298ef5e7fc92
VaheC <vcharchian@gmail.com>
2025-12-26 20:46:22 +0400

    initialized

---
+++ src/helper.py
@@ -0,0 +1,23 @@
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+def load_pdf_file(data):
+
+    data_loader = DirectoryLoader(
+        data,
+        glob='*.pdf',
+        loader_cls=PyPDFLoader
+    )
+    documents = data_loader.load()
+
+    return documents
+
+def get_text_chunks(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+def download_hf_embeddings(model_name="sentence-transformers/all-MiniLM-L6-v2"):
+    embeddings = HuggingFaceEmbeddings(model_name=model_name)
+    return embeddings
